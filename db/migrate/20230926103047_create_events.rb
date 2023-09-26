7cc11c1e4cece69b50e3fd42f642a8239a9c9798
Rajesh Sharma <rajesh@surf.net.np>
2023-09-26 12:31:59 +0200

    Add rails new app

---
+++ db/migrate/20230926103047_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[7.1]
+  def change
+    create_table :events do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :task, null: false, foreign_key: true
+      t.integer :point
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
